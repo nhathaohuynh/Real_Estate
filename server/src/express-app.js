25ac3519859399947517af2b2af45cb51fccc30b
S520H0531 <520H0531@student.tdtu.edu.vn>
2023-12-19 11:19:28 +0700

    set up project

---
+++ server/src/express-app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const { notFound } = require('./middleware/notFound')
+const { errorHandler } = require('./middleware/errorHandler')
+const morgan = require('morgan')
+
+module.exports = async (app) => {
+	app.use(express.urlencoded({ extended: true }))
+	app.use(express.json())
+	app.use(cors())
+	app.use(morgan('dev'))
+
+	// api
+	app.use('/property/api/v1', require('./router'))
+
+	// not found
+	app.use(notFound)
+
+	// error handling
+	app.use(errorHandler)
+}
---
+++ server/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const http = require('http')
+const expressApp = require('./express-app')
+
+const { PORT } = require('./config/env')
+
+class Server {
+	constructor() {
+		this.app = express()
+		this.server = http.createServer(this.app)
+		this.PORT = PORT || 8080
+	}
+
+	async start() {
+		await expressApp(this.app)
+		// await connectDB()
+		this.runServer().setupErrorHandling()
+	}
+
+	runServer() {
+		this.server.listen(this.PORT, () => {
+			console.log(`Server is running on port ${this.PORT}`)
+		})
+		return this
+	}
+
+	setupErrorHandling() {
+		this.app.on('error', (err) => {
+			console.error(err)
+			process.exit(1)
+		})
+	}
+}
+
+const serverInstance = new Server()
+
+serverInstance.runServer()
